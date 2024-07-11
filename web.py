dbebd14eff7838efd005c848817e19c220e6ba8e
acemi159 <61695430+acemi159@users.noreply.github.com>
2024-07-11 19:25:11 +0200

    Implement webapp interface with streamlit

---
+++ web.py
@@ -0,0 +1,33 @@
+import functions
+import streamlit as st
+import os 
+
+if not os.path.exists("todos.txt"):
+    with open("todos.txt", 'w') as file:
+        pass
+
+
+todos = functions.get_todos()
+
+def add_todo():
+    todo = st.session_state['new_todo'] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+    st.session_state['new_todo'] = ""
+    
+
+st.title("My Todo App")
+st.subheader("This is my Todo app.")
+st.write("This app is to increase your productivity.")
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+    
+        
+st.text_input(label="Enter a todo", placeholder="Add new todo...",
+              on_change=add_todo, key="new_todo")
